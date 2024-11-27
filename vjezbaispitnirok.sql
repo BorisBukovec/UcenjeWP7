3092505a80345989885045d17717e49bf3ba9c76
BorisBukovec <botaosijek@gmail.com>
2024-11-27 21:00:38 +0100

    nisam završio

---
+++ vjezbaispitnirok.sql
@@ -0,0 +1,22 @@
+﻿use master;
+go
+drop database if exists vjezbaispitnirok;
+go
+create database vjezbaispitnirok;
+go
+use vjezbaispitnirok;
+go
+
+create table IspitniRok(
+sifra int,
+predmet varchar(50),
+vrstaIspita varchar(50),
+datum datetime,
+pristupio int
+);
+
+create table Pristupnici(
+ispitniRok int,
+studnet varchar(50),
+brojBodova 
+);
