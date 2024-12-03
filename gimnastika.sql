be47285fab5d2be00eb15ddf3fd5356cb174c1f2
Boris Bukovec <botaosijek@gmail.com>
2024-12-03 11:25:30 +0100

    završni rad

---
+++ gimnastika.sql
@@ -0,0 +1,29 @@
+﻿use master;
+go
+
+drop database if exists gimnastika;
+go
+
+create database gimnastika;
+go
+
+use gimnastika;
+go
+
+create table klubovi(
+sifra int not null primary key identity(1,1),
+naziv varchar(100) not null,
+adresa varchar(100) not null,
+ziroracun int
+);
+
+create table vjezbaci(
+sifra int not null primary key identity(1,1),
+ime varchar(30) not null,
+prezime varchar(40) not null,
+datumrodenja datetime not null,
+spol varchar(20) not null,
+kategorija varchar(50) not null,
+email varchar(100),
+klub int not null references klubovi(sifra)
+);
