0461b65d032d5eaeab4e4278671c1b816d65a036
Boris Bukovec <botaosijek@gmail.com>
2024-12-15 10:54:03 +0100

    popracvci

--- gimnastika.sql
+++ gimnastika.sql
@@ -16,7 +16,7 @@ create table klubovi(
 sifra int not null primary key identity(1,1),
 naziv varchar(100) not null,
 adresa varchar(100) not null,
-IBAN varchar(50) not null
+iban varchar(50) not null
 );
 
 create table vjezbaci(
@@ -77,23 +77,23 @@ insert into klubovi(naziv,adresa,IBAN) values
 select * from vjezbaci;
 
 insert into vjezbaci
-(ime,prezime,datumrodenja,spol,kategorija,klub) values
-('Ana','Anić','2015-08-13','žensko','kadetkinje','Gd Osijek-žito');
+(ime,prezime,datumrodenja,spol,kategorija,email,klub) values
+('Ana','Anić','2015-08-13','žensko','kadetkinje','anaanic@gmail.com','Gd Osijek-žito');
 
 
-insert into vjezbaci(ime,prezime,datumrodenja,spol,kategorija,klub) values
+insert into vjezbaci(ime,prezime,datumrodenja,spol,kategorija,email,klub) values
 --2
-('Maria','Hes','2013-04-22','žensko','juniorke','ZTD Hrvatski sokol Zagreb'),
+('Maria','Hes','2013-04-22','žensko','juniorke','mariahes@gmail.com','ZTD Hrvatski sokol Zagreb'),
 --3
-('Ivan','Marković','2016-08-09','muško','kadet','Gimnastičko Društvo "Hrvatski Sokol" Valpovo'),
+('Ivan','Marković','2016-08-09','muško','kadet','ivanmarkovic@gmail.com','Gimnastičko Društvo "Hrvatski Sokol" Valpovo'),
 --4
-('Marko','Ivić','2016-07-17','muško','kadet','Gd Osijek-žito');
+('Marko','Ivić','2016-07-17','muško','kadet','markoivic@gmail.com','Gd Osijek-žito');
 
 select * from treneri;
 
 insert into treneri
-(ime,prezime,email,klub) values
-('Boris','Bukovec','botaosijek@gmail.com','Gimnastičko Društvo "Hrvatski Sokol" Valpovo');
+(ime,prezime,oib,email,klub) values
+('Boris','Bukovec',62839563744,'botaosijek@gmail.com','Gimnastičko Društvo "Hrvatski Sokol" Valpovo');
 
 insert into treneri(ime,prezime,email,klub) values
 --2
@@ -118,3 +118,9 @@ select * from natjecanja;
 insert into natjecanja
 (grad,adresa,datum,iznoskotizacije) values
 ('Osijek','Ul. Kneza Trpimira 23','2024-12-22 09:00:00',50);
+
+insert into natjecanja
+(grad,adresa,datum,iznoskotizacije) values
+--2
+('Zagreb','Ježdovečka ulica 3B','2025-02-13 09:00:00',50);
+
